b5b91fb68c0cea495a62b53860526ccafb9c968f
Muzzh <murdyn@gmail.com>
2017-07-01 17:06:32 +0000

    Added Delete button to edit page, and destroy function in controller (lesson 24)

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -25,10 +25,16 @@ class PlacesController < ApplicationController
 
   def update
     @place = Place.find(params[:id])
-    current_id = :id
     @place.update_attributes(place_params)
     redirect_to root_path
   end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
+  end
+
 
   def require_permission
     if current_user != Place.find(params[:id]).user
