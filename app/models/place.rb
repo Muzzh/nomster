7dc1a713cc6328615e5c41ef5bc5c1401742bcf0
Muzzh <murdyn@gmail.com>
2017-07-01 20:47:19 +0000

    completed validations for new places (lesson 28)

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }
+  validates :occurence, presence: true
+  validates :address, presence: true
+  validates :description, presence: true, length: { minimum: 20, maximum: 140 }  
 end
