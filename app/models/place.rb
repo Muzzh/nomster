b1741df59ddf60ba60f5f1e48b4e96fb7d0ceacd
Muzzh <murdyn@gmail.com>
2017-07-07 14:48:20 +0000

    prettied forms and fixed carousel caption tag

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,7 +6,7 @@ class Place < ApplicationRecord
   geocoded_by :address
   after_validation :geocode
 
-  validates :name, presence: true, length: { minimum: 3, maximum: 25 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
   validates :occurence, presence: true
   validates :address, presence: true
   validates :description, presence: true, length: { minimum: 20, maximum: 140 }  
